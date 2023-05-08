b1891e7546148d1316eba4317d105f0de01c8fac
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-08 18:54:29 +0300

    add React import

--- dist/assets/components/Emoji/Emoji.js
+++ dist/assets/components/Emoji/Emoji.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = __importDefault(require("react"));
 // @ts-ignore
 const emoji_data_json_1 = __importDefault(require("../../data/emoji-data.json"));
 /**
--- src/assets/components/Emoji/Emoji.tsx
+++ src/assets/components/Emoji/Emoji.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 import { EmojiName } from '../../../types/EmojiName';
 // @ts-ignore
